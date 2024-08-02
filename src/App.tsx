43a2c8e527060c3dbe583b14c5dae3874f46550a
Ali Marzoug <marzougali@isimsf.u-sfax.tn>
2024-08-02 11:53:06 +0100

    feat: "add react ts code in the App, App.test and index"

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SearchModal from './components/SearchModal';
+import NotFound from './components/NotFound';
+import Footer from './components/Footer';
+import Hero from './components/Hero'; 
+import Shop from './components/Shop'; 
+import Contact from './components/Contact'; 
+import './App.css'; // Your custom styles
+import ProductDetail from './components/ProductDetail';
+import Testimonial from './components/Testimonial';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import PageHeader from './components/PageHeader';
+
+const App: React.FC = () => {
+  const location = useLocation();
+    const getTitle = () => {
+        switch (location.pathname) {
+            case '/':
+                return 'Welcome';
+            case '/checkout':
+                return 'Checkout';
+            case '/cart':
+                return 'Your Cart';
+            case '/testimonial':
+                return 'Testimonials';
+            case '/productDetail':
+                return 'Product Details';
+            case '/shop':
+                return 'Shop';
+            case '/contact':
+                return 'Contact Us';
+            default:
+                return 'Page Not Found';
+        }
+    };
+    return (
+        <Router>
+            <Navbar />
+            <SearchModal />
+            <PageHeader title={getTitle()} />
+            <Routes>
+                <Route path="/" element={<Hero />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/testimonial" element={<Testimonial />} />
+                <Route path="/productDetail" element={<ProductDetail />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
+};
+
+export default App;
