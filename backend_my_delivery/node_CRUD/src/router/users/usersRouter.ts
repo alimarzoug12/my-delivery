d631794356ec88b4299d27c785148ba902146ca8
Ali Marzoug <marzougali@isimsf.u-sfax.tn>
2024-08-30 16:38:56 +0100

    add backend_my_delivery

---
+++ backend_my_delivery/node_CRUD/src/router/users/usersRouter.ts
@@ -0,0 +1,240 @@
+import express from "express";
+
+import { addUser, getUser, deleteUser, login, updateUser, signup } from "../../controllers/users/userController";
+
+
+
+const usersRouter = express.Router();
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: The books managing API
+ */
+
+/**
+ * @swagger
+ * /delete:
+ *   delete:
+ *     summary: delete User
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+usersRouter.route("/delete").delete(deleteUser);
+
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Add user
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ *
+ */
+usersRouter.route("/user").post(addUser);
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: get User
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+usersRouter.route("/users").get(getUser);
+
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: login
+ *     tags: [Login]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+usersRouter.route("/login").post(login);
+
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     summary: signup
+ *     tags: [signup]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+usersRouter.route("/signup").post(signup);
+
+
+/**
+ * @swagger
+ * /updateUser:
+ *   put:
+ *     summary: update user
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *              
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ *
+ */
+usersRouter.route("/updateUser").put(updateUser);
+
+export default usersRouter;
